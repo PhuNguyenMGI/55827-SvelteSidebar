1adacedb305b1ee900b1be9b3c2865858c27997d
Phu Nguyen <phu.nguyen@mgisolutions.com>
2023-08-28 09:57:22 +0700

    first commit

---
+++ src/lib/components/IconButton.svelte
@@ -0,0 +1,29 @@
+<script>
+import Happy from "$lib/images/happy-outline.svg";
+import Sad from "$lib/images/sad-outline.svg";
+import CodeSlash from "$lib/images/code-slash-outline.svg";
+import Sync from "$lib/images/sync-outline.svg";
+</script>
+
+<div class="sidebar">
+	<div class="item" data-tooltip="Item 1 🤔">
+		<div class="circle">
+            <img src={Happy} class="icon" alt="Happy face">
+        </div>
+	</div>
+	<div class="item" data-tooltip="Item 2 🤔">
+		<div class="circle">
+            <img src={CodeSlash} class="icon" alt="Code slash">
+        </div>
+	</div>
+	<div class="item" data-tooltip="Item 3 🤔">
+		<div class="circle">
+            <img src={Sad} class="icon" alt="Sad face">
+        </div>
+	</div>
+	<div class="item" data-tooltip="Item 4 🤔">
+		<div class="circle">
+            <img src={Sync} class="icon" alt="Sync">
+        </div>
+	</div>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+	import IconButton from '$lib/components/IconButton.svelte';
+</script>
+
+<section>
+	<IconButton />
+	<div class="novel ml-[60px] px-6">
+		<h1 class="chapter-title">Chapter 1: The Lorem Ipsum Novel</h1>
+		<p class="paragraph">
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, lacus nec vestibulum
+			tempor, lectus nunc sollicitudin tortor, in lacinia mauris mi nec tellus.
+		</p>
+		<p class="paragraph">
+			Sed gravida feugiat est eu efficitur. Fusce hendrerit ex non est tempor, ut maximus sapien
+			fringilla. Nam sit amet justo id quam auctor varius.
+		</p>
+		<p class="paragraph">
+			Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec
+			ullamcorper enim et nisl consectetur, in porttitor purus ultricies.
+		</p>
+	</div>
+</section>
+
+<style></style>
